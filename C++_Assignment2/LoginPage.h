c5a423057f5ca117b3ba12c8ab91cb4f67a1ac9a
zzeyadnader <zeyadnader00@gmail.com>
2025-12-14 20:51:00 +0200

    Loginpage design

--- C++_Assignment2/LoginPage.h
+++ C++_Assignment2/LoginPage.h
@@ -34,6 +34,15 @@ namespace C___Assignment2 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Panel^ panel1;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Label^ label4;
+	protected:
 
 	private:
 		/// <summary>
@@ -48,12 +57,127 @@ namespace C___Assignment2 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm1";
-			this->Padding = System::Windows::Forms::Padding(0);
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm1::typeid));
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->panel1->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// panel1
+			// 
+			this->panel1->BackColor = System::Drawing::Color::DarkMagenta;
+			this->panel1->Controls->Add(this->button1);
+			this->panel1->Controls->Add(this->label4);
+			this->panel1->Controls->Add(this->textBox2);
+			this->panel1->Controls->Add(this->textBox1);
+			this->panel1->Controls->Add(this->label3);
+			this->panel1->Controls->Add(this->label2);
+			this->panel1->Controls->Add(this->label1);
+			this->panel1->Location = System::Drawing::Point(122, 62);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(624, 366);
+			this->panel1->TabIndex = 0;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 26.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->ForeColor = System::Drawing::SystemColors::ButtonHighlight;
+			this->label1->Location = System::Drawing::Point(254, 25);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(106, 39);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Login";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 21.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->ForeColor = System::Drawing::SystemColors::ButtonHighlight;
+			this->label2->Location = System::Drawing::Point(50, 128);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(191, 33);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"Student Code";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 21.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->ForeColor = System::Drawing::SystemColors::ButtonHighlight;
+			this->label3->Location = System::Drawing::Point(34, 209);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(234, 33);
+			this->label3->TabIndex = 2;
+			this->label3->Text = L"National Number";
+			this->label3->Click += gcnew System::EventHandler(this, &MyForm1::label3_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(274, 128);
+			this->textBox1->Multiline = true;
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(305, 33);
+			this->textBox1->TabIndex = 3;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(274, 209);
+			this->textBox2->Multiline = true;
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(305, 33);
+			this->textBox2->TabIndex = 4;
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label4->ForeColor = System::Drawing::SystemColors::ButtonHighlight;
+			this->label4->Location = System::Drawing::Point(467, 263);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(126, 24);
+			this->label4->TabIndex = 5;
+			this->label4->Text = L"Forgot Code\?";
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->ForeColor = System::Drawing::Color::DarkMagenta;
+			this->button1->Location = System::Drawing::Point(207, 299);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(229, 49);
+			this->button1->TabIndex = 6;
+			this->button1->Text = L"Login";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// MyForm1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->ClientSize = System::Drawing::Size(872, 493);
+			this->Controls->Add(this->panel1);
+			this->Name = L"MyForm1";
+			this->Text = L"MyForm1";
+			this->panel1->ResumeLayout(false);
+			this->panel1->PerformLayout();
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
-	};
+	private: System::Void label3_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+};
 }
